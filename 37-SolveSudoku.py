b9af9f225d29e9a43cc0f50352f2f5a2e97a7b87
Yoc Lu <searcher408@gmail.com>
2021-06-14 14:07:01 +0800

    Add some files.

--- 37-SolveSudoku.py
+++ 37-SolveSudoku.py
@@ -1,7 +1,16 @@
-# Python
+# 题目数据保证输入数独仅有一个解。
+# 按照「行优先」的顺序依次枚举每一个空白格中填的数字，通过递归 + 回溯的方法枚举所有可能的填法。
+# 当递归到最后一个空白格后，如果仍然没有冲突，说明找到了答案；
+# 在递归的过程中，如果当前的空白格不能填下任何一个数字，那么就进行回溯。
+
+# 由于所给出的解题方法均以递归 + 回溯为基础，算法运行的时间（以及时间复杂度）很大程度取决于给定的输入数据，
+# 很难找到一个非常精确的渐进紧界。因此这里只给出一个较为宽松的渐进复杂度上界O(9^(9×9))，
+# 即最多有 9×9 个空白格，每个格子可以填 [1,9] 中的任意整数。
+
+
+# Python 递归
 from typing import List
 
-# 递归
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
@@ -11,7 +20,7 @@ class Solution:
             nonlocal valid
             if pos == len(spaces):
                 valid = True
-                return
+                return # 只返回一次，即pos等于空格数的时候
             
             i, j = spaces[pos]
             for digit in range(9):
@@ -20,7 +29,7 @@ class Solution:
                     raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True
                     dfs(pos + 1)
                     raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = False
-                if valid:
+                if valid: # 用于递归结束后每层递归的返回，返回次数即递归深度，即空格数
                     return
         
         raw = [[False] * 9 for _ in range(9)]
@@ -35,57 +44,15 @@ class Solution:
                     spaces.append((i, j))
                 else:
                     digit = int(board[i][j]) - 1
-                    raw[i][digit] = column[j][digit] = block[i // 3][j // 3] = True
+                    raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True
         
         dfs(0)
 
-class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
-        def dfs(pos: int):
-            nonlocal valid
-            if pos == len(spaces):
-                valid = True
-                return
-            
-            i, j = spaces[pos]
-            for digit in range(9):
-                if raw[i][digit] == column[j][digit] == block[i // 3][j // 3][digit] == False:
-                    board[i][j] = str(digit + 1)
-                    raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True
-                    dfs(pos + 1)
-                    raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = False
-                if valid:
-                    return
-            
-        raw = [[False] * 9 for _ in range(9)]
-        column = [[False] * 9 for _ in range(9)]
-        block = [[[False] * 9 for _c in range(3)] for _r in range(3)]
-        valid = False
-        spaces = list()
-
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == ".":
-                    spaces.append((i, j))
-                else:
-                    digit = int(board[i][j]) - 1
-                    raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True
-
-        dfs(0)
-
-
-# 按照「行优先」的顺序依次枚举每一个空白格中填的数字，通过递归 + 回溯的方法枚举所有可能的填法。
-# 当递归到最后一个空白格后，如果仍然没有冲突，说明找到了答案；
-# 在递归的过程中，如果当前的空白格不能填下任何一个数字，那么就进行回溯。
-
-# 由于所给出的解题方法均以递归 + 回溯为基础，算法运行的时间（以及时间复杂度）很大程度取决于给定的输入数据，
-# 很难找到一个非常精确的渐进紧界。因此这里只给出一个较为宽松的渐进复杂度上界O(9^(9×9))，
-# 即最多有 9×9 个空白格，每个格子可以填 [1,9] 中的任意整数。
 
 # C++ 递归
 class Solution {
 private:
-    bool line[9][9]; // 一共9行，标记每行中9个数字出现情况
+    bool raw[9][9]; // 一共9行，标记每行中9个数字出现情况
     bool column[9][9]; // 一共9列，标记每列中9个数字出现情况
     bool block[3][3][9]; // 一共3*3个区块，标记每个block中9个数字出现情况
     bool valid; // 标记是否还有空白格进行递归
@@ -99,19 +66,19 @@ public:
         }
 
         auto [i, j] = spaces[pos];
-        for (int digit = 0; digit < 9 && !valid; digit++) {
-            if (!line[i][digit] && !column[j][digit] && !block[i / 3][j / 3][digit]) {
+        for (int digit = 0; digit < 9 && !valid; digit++) { // 一旦valid变为true，即达到最大递归深度找到答案，就不再进入循环
+            if (!raw[i][digit] && !column[j][digit] && !block[i / 3][j / 3][digit]) {
                 board[i][j] = '0' + digit + 1; // 填入了数字之后，要将标记的三个值都置为true
-                line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                raw[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
                 dfs(board, pos + 1); // 继续对下一个空白格位置进行递归
-                line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = false;
+                raw[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = false;
                 // 在回溯到当前递归层时，还要将标记的三个值重新置为false。
             }
         }
     }
 
     void solveSudoku(vector<vector<char>>& board) {
-        memset(line, false, sizeof(line));
+        memset(raw, false, sizeof(raw));
         memset(column, false, sizeof(column));
         memset(block, false, sizeof(block));
         valid = false;
@@ -122,7 +89,7 @@ public:
                     spaces.emplace_back(i, j); // emplace_back() 的执行效率比 push_back() 高
                 } else {
                     int digit = board[i][j] - '0' - 1;
-                    line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                    raw[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
                 }
             }
         }
@@ -130,3 +97,164 @@ public:
         dfs(board, 0);
     }
 };
+
+
+# 位运算优化
+# 方法一中使用了长度为 9 的数组表示每个数字是否出现过。同样也可以借助位运算，仅使用一个整数表示每个数字是否出现过。
+# 具体地，数 b 的二进制表示的第 i 位（从低到高，最低位为第 0 位）为 1，当且仅当数字 i+1 已经出现过。
+# 例如当 b 的二进制表示为 (011000100)_2时，就表示数字 3，7，8 已经出现过。
+
+# C++
+class Solution {
+private:
+    int raw[9]; // 每个数字表示相应行中9个数字的出现情况
+    int column[9]; // 每个数字表示相应列中9个数字的出现情况
+    int block[3][3]; // 每个数字表示相应block中9个数字的出现情况
+    bool valid;
+    vector<pair<int, int>> spaces;
+
+public:
+    void flip(int i, int j, int digit) { // flip 快速翻转
+        raw[i] ^= (1 << digit); // 与数 1 << i 进行按位异或运算，将第 i 位从 0 变为 1，或从 1 变为 0
+        column[j] ^= (1 << digit); 
+        block[i / 3][j / 3] ^= (1 << digit);
+    }
+
+    void dfs(vector<vector<char>>& board, int pos) {
+        if (pos == spaces.size()) {
+            valid = true;
+            return;
+        }
+
+        auto [i, j] = spaces[pos];
+        int mask = ~(raw[i] | column[j] | block[i / 3][j / 3]) & 0x1ff; // 0x1ff 低9位为1，高位为0
+        // 对于第 i 行第 j 列的位置, (raw[i] | column[j] | block[i / 3][j / 3])中第 k 位为 1，表示该位置不能填入数字 k+1
+        // 对这个值进行按位取反运算，那么第 k 位为 1 就表示该位置可以填入数字k+1，就可以通过寻找 1 来进行枚举。
+        // 由于在进行按位取反运算后，这个数的高位也全部变成了 1, 
+        // 将这个数和 (1 1111 1111)_2 = (1FF)_16进行按位与运算，将所有无关的位设置为0。
+
+        for (; mask && !valid; mask &= (mask - 1)) { 
+            int digitMask = mask & (-mask); // 用 b & (−b) 得到 b 二进制表示中最低位的 1，因为(-b)以补码的形式存储，等于∼b+1
+            int digit = __builtin_ctz(digitMask); // 通过语言自带的函数得到这个最低位的 1 究竟是第几位（即 i 值）
+            flip(i, j, digit);
+            board[i][j] = '0' + digit + 1;
+            dfs(board, pos + 1);
+            flip(i, j, digit);
+
+            // 用 b 和最低位的 1 进行按位异或运算，就可以将其从 b 中去除，这样就可以枚举下一个 1。
+            // 同样地也可以用 b 和 b−1 进行按位与运算达到相同的效果: mask &= (mask - 1)
+        }
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        memset(raw, 0, sizeof(raw));
+        memset(column, 0, sizeof(column));
+        memset(block, 0, sizeof(block));
+        valid = false;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int digit = board[i][j] - '0' - 1;
+                    flip(i, j, digit);
+                }
+            }
+        }
+
+        // 枚举优化，将唯一确定的空白格全部填入对应的数。
+        while (true) {
+            int modified = false;
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (board[i][j] == '.') {
+                        int mask = ~(raw[i] | column[j] | block[i / 3][j / 3]) & 0x1ff;
+                        if (!(mask & (mask - 1))) {
+                            int digit = __builtin_ctz(mask);
+                            flip(i, j, digit);
+                            board[i][j] = '0' + digit + 1;
+                            modified = true;
+                        }
+                    }
+                }
+            }
+            if (!modified) {
+                break; // 跳出循环
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    spaces.emplace_back(i, j);
+                }
+            }
+        }
+
+        dfs(board, 0);
+    }
+};
+
+# 枚举优化
+# 顺着方法二的思路继续优化，如果一个空白格只有唯一的数可以填入，也就是其对应的 b 值和 b-1 进行按位与运算后得到 0
+# （即 b 中只有一个二进制位为 1）。此时就可以确定这个空白格填入的数，而不用等到递归时再去处理它。
+# 可以首先遍历整个数独，将唯一确定的空白格全部填入对应的数。随后再使用与方法二相同的方法对剩余无法唯一确定的空白格进行递归 + 回溯。
+
+# Python3
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        def flip(i: int, j: int, digit: int):
+            raw[i] ^= (1 << digit)
+            column[j] ^= (1 << digit)
+            block[i // 3][j // 3] ^= (1 << digit)
+
+        def dfs(pos: int):
+            nonlocal valid
+            if pos == len(spaces):
+                valid = True
+                return
+            
+            i, j = spaces[pos]
+            mask = ~(raw[i] | column[j] | block[i // 3][j // 3]) & 0x1ff
+            while mask:
+                digitMask = mask & (-mask) # 得到最低位的 1
+                digit = bin(digitMask).count('0') - 1 # 得到这个最低位的 1 究竟是第几位，bin(x)将整数转换为前缀为"0b"的二进制字符串
+                board[i][j] = str(digit + 1)
+                flip(i, j, digit)
+                dfs(pos + 1)
+                flip(i, j, digit)
+                mask &= (mask - 1)
+                if valid:
+                    return
+            
+        raw = [0] * 9
+        column = [0] * 9
+        block = [[0] * 3 for _ in range(3)]
+        valid = False
+        spaces = list()
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != ".":
+                    digit = int(board[i][j]) - 1
+                    flip(i, j, digit)
+        
+        while True:
+            modified = False
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j] == ".":
+                        mask = ~(raw[i] | column[j] | block[i // 3][j // 3]) & 0x1ff
+                        if not (mask & (mask - 1)):
+                            digit = bin(mask).count("0") - 1
+                            flip(i, j, digit)
+                            board[i][j] = str(digit + 1)
+                            modified = True
+            if not modified:
+                break
+        
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    spaces.append((i, j))
+
+        dfs(0)
--- 39-CombinationSum.py
+++ 39-CombinationSum.py
@@ -1,33 +1,164 @@
+# Python3
+from typing import List
+
+# 这一类问题都需要先画出树形图，然后编码实现。
+# 搜索回溯 不含剪枝优化
 class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
-        def dfs(pos: int):
-            nonlocal valid
-            if pos == len(spaces):
-                valid = True
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(candidates, target, begin, path, res):
+            if target == 0:
+                res.append(path)
                 return
             
-            i, j = spaces[pos]
-            for digit in range(9):
-                if raw[i][digit] == column[j][digit] == block[i // 3][j // 3][digit] == False:
-                    board[i][j] = str(digit + 1)
-                    raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True
-                    dfs(pos + 1)
-                    raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = False
-                if valid:
-                    return
-            
-        raw = [[False] * 9 for _ in range(9)]
-        column = [[False] * 9 for _ in range(9)]
-        block = [[[False] * 9 for _c in range(3)] for _r in range(3)]
-        valid = False
-        spaces = list()
+            for i in range(begin, len(candidates)):
+                if target - candidates[i] < 0:
+                    break
+                dfs(candidates, target - candidates[i], i, path + [candidates[i]], res)
 
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == ".":
-                    spaces.append((i, j))
-                else:
-                    digit = int(board[i][j]) - 1
-                    raw[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True
+        candidates.sort();
+        res = list()
+        path = list()
+        dfs(candidates, target, 0, path, res)
+        return res
 
-        dfs(0)
+# Python3 的 [1, 2] + [3] 语法生成了新的列表，一层一层传到根结点以后，直接 res.append(path) 就可以了；
+# 基本类型变量在传参的时候是复制，因此变量值的变化在参数里体现就行，所以 Python3 的代码看起来没有「回溯」这个步骤。
+
+
+# C++ 根据一个数选和不选画树形图
+# 时间复杂度：O(S)，其中 S 为所有可行解的长度之和。从分析给出的搜索树可以看出时间复杂度取决于搜索树所有叶子节点的深度之和，
+# 即所有可行解的长度之和。在这题中很难给出一个比较紧的上界，O(n×2^n) 是一个比较松的上界。
+# 空间复杂度：O(target)。除答案数组外，空间复杂度取决于递归的栈深度，在最差情况下需要递归 O(target) 层。
+class Solution {
+public:
+    void dfs(vector<int>& candidates, int target, vector<vecot<int>>& ans, vector<int>& combine, int idx) {
+        if (idx == candidates.size()) {
+            return;
+        }
+
+        if (target == 0) {
+            ans.emplace_back(combine); // 记录所有可行解
+            return;
+        }
+
+        // 直接跳过candidates中当前idx的数
+        dfs(candidates, target, ans, combine, idx + 1);
+
+        // 选择当前idx的数，添加到combine
+        if (target - candidates[idx] >= 0) {
+            combine.emplace_back(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx); // idx不变，可以重复选取当前idx的数
+            combine.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> combine;
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+};
+
+# C++ 使用lambda语法糖
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> path;
+        int sum = 0, n = candidates.size();
+
+        // std::function<void(int)> f; // 这里表示function的对象f的参数是int，返回值是void
+        function<void(int)> dfs = [&] (int begin) {
+            if (sum == target) {
+                res.emplace_back(begin(path), end(path));
+            } else if (sum < target) {
+                for (int i = begin; i != n; i++) {
+                    path.emplace_back(candidates[i]);
+                    sum += candidates[i];
+                    dfs(i);
+                    sum -= candidates[i];
+                    path.pop_back();
+                }       
+            } else {
+                return; // sum > target结束回溯
+            }
+        };
+
+        dfs(0);
+        return res;
+    }
+};
+
+# Java 对candidates排序，减少回溯次数
+# 每一次搜索的时候设置下一轮搜索的起点 begin，从而在搜索的时候对所得解去重
+# 备注：如果题目要求，结果集不计算顺序，此时需要按顺序搜索，才能做到不重不漏。
+public class Solution {
+    private List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> path = new ArrayList<>();
+        Array.sort(candidates);
+        backtrack(path, candidates, target, 0, 0);
+        return res;
+    }
+
+    private void backtrack(List<Integer> path, int[] candidates, int target, int sum, int begin) {
+        if (sum == target) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i < candidates.length; i++) {
+            int rs = candidates[i] + sum;
+            if (rs <= target) {
+                path.add(candidates[i]);
+                backtrack(path, candidates, target, rs, i);
+                path.remove(path.size() - 1);
+            } else {
+                break; // 当rs>target时是直接跳出循环
+            }
+        }
+    }
+}
+
+# 完整Java代码，以 target 为 根结点 ，创建一个分支的时做减法，画出树形图
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        // 排序剪枝
+        Arrays.sort(candidates);
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(candidates, target, 0, path, res);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int target, int begin, Deque<Integer> path, List<List<Inter>> res) {
+        // 由于进入更深层回溯时，小于0的部分被剪枝，因此递归终止条件值只判断等于0的情况
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i < candidates.length; i++) {
+            if (target - candidates[i] < 0) {
+                break;
+            }
+
+            path.addLast(candidates[i]);
+            dfs(candidates, target - candidates, i, path, res);
+            path.removeLast();
+        }
+    }
+}
