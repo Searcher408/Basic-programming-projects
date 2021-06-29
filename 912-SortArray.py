39be2c011e8f521347a1fc577c6918e1afbb2947
Yoc Lu <searcher408@gmail.com>
2021-06-29 16:08:00 +0800

    Add some files.

--- 912-SortArray.py
+++ 912-SortArray.py
@@ -109,3 +109,77 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+// 最大的元素放在末尾
+void bubbleSort(int *arr, int size) {
+    int i, j, tmp;
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
+
+// 每次循环找最小元素的索引
+void selectionSort(int *arr, int size) {
+    int i, j, k, tmp;
+    for (i = 0; i < size - 1; i++) {
+        k = i;
+        for (j = i + 1; j < size; j++) {
+            if (arr[j] < arr[k]) {
+                k = j;
+            }
+        }
+        tmp = arr[k];
+        arr[k] = arr[i];
+        arr[i] = tmp;
+    }
+}
+
+void insertionSort(int *arr, int size) {
+    int i, j, tmp;
+    for (i = 1; i < size; i++) {
+        if (arr[i] < arr[i - 1]) {
+            tmp = arr[i];
+            for (j = i - 1; j >= 0 && arr[j] > tmp; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = tmp;
+        }
+    }
+}
+
+// 快速排序
+void quickSort(int *arr, int maxlen, int begin, int end) {
+    int i, j;
+    if (begin < end) {
+        i = begin + 1;
+        j = end;
+        while (i < j) {
+            if (arr[i] > arr[begin]) {
+                swap(&arr[i], &arr[j]);
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        if (arr[i] >= arr[begin]) {
+            i--;
+        }
+        swap(&arr[begin], &arr[i]);
+        quickSort(arr, maxlen, begin, i);
+        quickSort(arr, maxlen, j, end);
+    }
+}
+
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
